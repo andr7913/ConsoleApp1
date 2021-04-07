6785d3431969c94c89dfa932ca8ab55ece33f66b
andr7913 <42999988+andr7913@users.noreply.github.com>
2021-04-07 10:02:01 +0200

    redo something

--- anagramsolver/Ianagramsolver.cs
+++ anagramsolver/Ianagramsolver.cs
@@ -1,7 +1,13 @@
-﻿namespace anagramsolver
+﻿using System.Collections.Generic;
+
+namespace anagramsolver
 {
     public interface Ianagramsolver
     {
-        
+        public void SetDictionary(List<string> dictionary);
+        public string Getbestvalueword(List<char> letters);
+        public List<string> getTopwords(List<char> letters, int Topcount);
+        public void addWord(string newWord);
+        public void removeWord(string removeWord);
     }
 }
--- anagramsolver/anagramsolver.cs
+++ anagramsolver/anagramsolver.cs
@@ -1,7 +1,32 @@
-﻿namespace anagramsolver
+﻿using System.Collections.Generic;
+
+namespace anagramsolver
 {
-    public class anagramsolver
+    public class anagramsolver:Ianagramsolver
     {
-        
+        public void SetDictionary(List<string> dictionary)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Getbestvalueword(List<char> letters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> getTopwords(List<char> letters, int Topcount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void addWord(string newWord)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void removeWord(string removeWord)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
